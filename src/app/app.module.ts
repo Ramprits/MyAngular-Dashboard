f105582f375446f8b6637bc64eda05ddd6adb303
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2017-10-26 10:06:53 +0530

    Re-arrange app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { MenuModule, PanelModule, ChartModule, InputTextModule, ButtonModule, InputMaskModule, InputTextareaModule, EditorModule, CalendarModule, RadioButtonModule, FieldsetModule, DropdownModule, MultiSelectModule, ListboxModule, SpinnerModule, SliderModule, RatingModule, DataTableModule, ContextMenuModule, TabViewModule, DialogModule, StepsModule, ScheduleModule, TreeModule, GMapModule, DataGridModule, TooltipModule, ConfirmationService, ConfirmDialogModule, GrowlModule, DragDropModule, GalleriaModule } from 'primeng/primeng';
+import { MenuModule } from 'primeng/primeng';
+import { PanelModule } from 'primeng/primeng';
+import { ChartModule } from 'primeng/primeng';
+import { InputTextModule } from 'primeng/primeng';
+import { ButtonModule } from 'primeng/primeng';
+import { InputMaskModule } from 'primeng/primeng';
+import { InputTextareaModule } from 'primeng/primeng';
+import { EditorModule } from 'primeng/primeng';
+import { CalendarModule } from 'primeng/primeng';
+import { RadioButtonModule } from 'primeng/primeng';
+import { FieldsetModule } from 'primeng/primeng';
+import { DropdownModule } from 'primeng/primeng';
+import { MultiSelectModule } from 'primeng/primeng';
+import { ListboxModule } from 'primeng/primeng';
+import { SpinnerModule } from 'primeng/primeng';
+import { SliderModule } from 'primeng/primeng';
+import { RatingModule } from 'primeng/primeng';
+import { DataTableModule } from 'primeng/primeng';
+import { ContextMenuModule } from 'primeng/primeng';
+import { TabViewModule } from 'primeng/primeng';
+import { DialogModule } from 'primeng/primeng';
+import { StepsModule } from 'primeng/primeng';
+import { ScheduleModule } from 'primeng/primeng';
+import { TreeModule } from 'primeng/primeng';
+import { GMapModule } from 'primeng/primeng';
+import { DataGridModule } from 'primeng/primeng';
+import { TooltipModule } from 'primeng/primeng';
+import { ConfirmationService } from 'primeng/primeng';
+import { ConfirmDialogModule } from 'primeng/primeng';
+import { GrowlModule } from 'primeng/primeng';
+import { DragDropModule } from 'primeng/primeng';
+import { GalleriaModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from "@angular/router";
+import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StatisticComponent } from './statistic/statistic.component';
 import { TimesheetComponent } from './timesheet/timesheet.component';
@@ -15,7 +45,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { SettingsComponent } from './settings/settings.component';
 
 import { AlltimesComponent } from './alltimes/alltimes.component';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FielderrorsComponent } from './fielderrors/fielderrors.component';
 import { CustomerService } from './services/customer.service';
 import { ProductListComponent } from './components/product/product-list/product-list.component';
@@ -31,13 +61,13 @@ import { EmployeeService } from './services/employee.service';
 
 
 const appRoutes: Routes = [
-  { path: "", redirectTo: "/dashboard", pathMatch: "full" },
-  { path: "dashboard", component: DashboardComponent },
-  { path: "alltimes", component: AlltimesComponent },
-  { path: "timesheet", component: TimesheetComponent },
-  { path: "projects", component: ProjectsComponent },
-  { path: "employees", component: EmployeeListComponent },
-  { path: "settings", component: SettingsComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'alltimes', component: AlltimesComponent },
+  { path: 'timesheet', component: TimesheetComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'settings', component: SettingsComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'fruits', component: FruitComponent },
 ];
